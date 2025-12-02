94de893d82752d47da0af96dd4064955397f7ad2
utahphotohunter <mitch.hunter1197@gmail.com>
2025-12-02 00:09:58 -0700

    made first dynamic content in React component.

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,10 +1,19 @@
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+import reactCoreConceptsImg from "./assets/components.png";
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
+
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactCoreConceptsImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
